c2d3aab97f5c7a823862c5b63a3a8a9fba1f40a7
Vytautas Ziurlis <vytautas.ziurlis@zopa.com>
2019-07-01 14:54:14 +0100

    Added tests

---
+++ Tests/Karmr.DomainUnitTests/Denormalizers/DenormalizerTests.cs
@@ -0,0 +1,49 @@
+﻿using Karmr.Common.Contracts;
+using Karmr.Common.Infrastructure;
+using Karmr.Domain.Denormalizers;
+using Moq;
+using NUnit.Framework;
+
+namespace Karmr.DomainUnitTests.Denormalizers
+{
+    [TestFixture]
+    public class DenormalizerTests
+    {
+        [Test]
+        public void ApplyingEventCallsConcreteMethod()
+        {
+            var subject = new DummyDenormalizer(Mock.Of<IDenormalizerRepository>());
+
+            var @event = new DummyEvent1();
+            subject.Apply(@event);
+
+            Assert.AreSame(@event, subject.lastAppliedEvent);
+        }
+
+        [Test]
+        public void DenormalizerThrowsWhenApplyingUnhandledEvent()
+        {
+            var subject = new DummyDenormalizer(Mock.Of<IDenormalizerRepository>());
+
+            var @event = new DummyEvent2();
+
+            Assert.Throws<UnhandledEventException>(() => subject.Apply(@event));
+        }
+
+        private class DummyDenormalizer : Denormalizer
+        {
+            public IEvent lastAppliedEvent;
+
+            public DummyDenormalizer(IDenormalizerRepository repository) : base(repository) { }
+
+            private void Apply(DummyEvent1 @event)
+            {
+                this.lastAppliedEvent = @event;
+            }
+        }
+
+        private class DummyEvent1 : IEvent { }
+
+        private class DummyEvent2 : IEvent { }
+    }
+}
---
+++ Tests/Karmr.DomainUnitTests/Denormalizers/ListingDenormalizerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Karmr.Common.Contracts;
+using Karmr.Domain.Denormalizers;
+using Karmr.Domain.Events;
+using Moq;
+using NUnit.Framework;
+using Karmr.DomainUnitTests.Helpers;
+
+namespace Karmr.DomainUnitTests.Denormalizers
+{
+    [TestFixture]
+    public class ListingDenormalizerTests
+    {
+        private readonly Mock<IDenormalizerRepository> repository;
+        private readonly ListingDenormalizer subject;
+
+        public ListingDenormalizerTests()
+        {
+            this.repository = new Mock<IDenormalizerRepository>();
+            this.subject = new ListingDenormalizer(this.repository.Object);
+        }
+
+        [Test]
+        public void ApplyingListingCreatedEventCallsRepository()
+        {
+            var @event = new ListingCreated(Guid.NewGuid(), Guid.NewGuid(), "Description", DateTime.Now);
+
+            var expectedParams = new
+            {
+                @event.EntityKey,
+                @event.UserId,
+                @event.Description,
+                @event.Timestamp
+            };
+
+            this.subject.Apply(@event);
+
+            this.repository.Verify(
+                x => x.Execute(
+                    "INSERT INTO ReadModel.Listing ([Id], [UserId], [Description], [Created]) VALUES (@EntityKey, @UserId, @Description, @Timestamp)",
+                    It.Is<object>(@params => Asserts.HaveSameProperties(expectedParams, @params))),
+                Times.Once);
+        }
+
+        [Test]
+        public void ApplyingListingUpdatedEventCallsRepository()
+        {
+            var @event = new ListingUpdated(Guid.NewGuid(), Guid.NewGuid(), "Description", DateTime.Now);
+
+            var expectedParams = new
+            {
+                @event.EntityKey,
+                @event.Description,
+                @event.Timestamp
+            };
+
+            this.subject.Apply(@event);
+
+            this.repository.Verify(
+                x => x.Execute(
+                    "UPDATE ReadModel.Listing SET [Description] = @Description, [Modified] = @Timestamp WHERE Id = @EntityKey",
+                    It.Is<object>(@params => Asserts.HaveSameProperties(expectedParams, @params))),
+                Times.Once);
+        }
+    }
+}
