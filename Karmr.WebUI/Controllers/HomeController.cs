4371767ac0d7307d7dae656896d3d6f785a5fe24
Vytautas Ziurlis <vytautas.ziurlis@zopa.com>
2019-08-14 13:48:15 +0100

    Add web UI project

---
+++ Karmr.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+using Karmr.Domain.Queries;
+using Karmr.Persistence;
+
+namespace Karmr.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TestPage()
+        {
+            var query = new ListingQueries(new QueryRepository("Server=.;Database=Karmr;User Id=Karmr;Password=Karmr;"));
+            var listings = query.GetAll();
+            return View();
+        }
+    }
+}
