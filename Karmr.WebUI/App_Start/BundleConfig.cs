e36c6b39c8f1ec8b46d2b74da05802872eb91361
Vytautas Ziurlis <vytautas.ziurlis@zopa.com>
2019-08-16 14:23:52 +0100

    Add HTML template

--- Karmr.WebUI/App_Start/BundleConfig.cs
+++ Karmr.WebUI/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Karmr.WebUI
 {
@@ -8,23 +7,45 @@ namespace Karmr.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region OldTheme
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/old/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/old/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/old/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                "~/Scripts/old/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/cssOld").Include(
+                "~/Content/old/bootstrap.css",
+                "~/Content/old/site.css"));
+
+            #endregion
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery.js",
+                "~/Scripts/jquery.collapsible.min.js",
+                "~/Scripts/swiper.min.js",
+                "~/Scripts/jquery.countdown.min.js",
+                "~/Scripts/circle-progress.min.js",
+                "~/Scripts/jquery.countTo.min.js",
+                "~/Scripts/jquery.barfiller.js",
+                "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/elegant-fonts.css",
+                "~/Content/themify-icons.css",
+                "~/Content/swiper.min.css",
+                "~/Content/style.css"));
         }
     }
 }
---
+++ Karmr.WebUI/Controllers/ThemeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+
+namespace Karmr.WebUI.Controllers
+{
+    public class ThemeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Causes()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Elements()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult SingleCauses()
+        {
+            return View();
+        }
+    }
+}
