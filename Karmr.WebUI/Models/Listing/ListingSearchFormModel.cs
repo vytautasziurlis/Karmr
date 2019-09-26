a4a6ceca5952e46d6b33dd33d722d8642b16cb01
Vytautas Ziurlis <vytautas.ziurlis@zopa.com>
2019-09-26 14:33:14 +0100

    Implemented first draft of the listing search page

---
+++ Karmr.WebUI/Models/Listing/ListingSearchFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Karmr.WebUI.Models.Listing
+{
+    public class ListingSearchFormModel
+    {
+        [Required(ErrorMessage = "Please enter your location to continue")]
+        public decimal? Latitude { get; set; }
+
+        [Required(ErrorMessage = "Please enter your location to continue")]
+        public decimal? Longitude { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ Karmr.WebUI/Models/Listing/ListingSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Karmr.WebUI.Models.Listing
+{
+    public class ListingSearchViewModel
+    {
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public string Address { get; set; }
+    }
+}
